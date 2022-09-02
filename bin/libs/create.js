742eb85aae5743838529aca5871c762a69584364
murukal <tutu@fantufantu.com>
2022-09-02 22:30:54 +0800

    feat: change gitignore

--- bin/libs/create.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.create = void 0;
-const create = () => {
-    console.log('1111');
-};
-exports.create = create;
