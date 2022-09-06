43d4fe9ce9359b5f11966ee22b156101b67529c7
hujiazhan.hjz <hujiazhan.hjz@alibaba-inc.com>
2022-09-06 10:24:42 +0800

    feat: add chalk for log display

--- src/libs/create.ts
+++ src/libs/create.ts
@@ -1,22 +1,24 @@
 // third
 import inquirer from 'inquirer'
 import yeoman from 'yeoman-environment'
+import chalk from 'chalk'
 // project
 import { PROJECT_TEMPLATE_CHOICE, TELEPORT_CHOICE } from '../assets/create.js'
 
 export const create = async () => {
   // 选项
   const createOptions = await inquirer.prompt([TELEPORT_CHOICE, PROJECT_TEMPLATE_CHOICE])
-
   // 创建yeoman运行时
   const env = yeoman.createEnv()
   // 运行时：下载以来
   const isInstalled = await env.installLocalGenerators({
     'generator-fantu': ''
   })
-
-  console.log('isInstalled====', isInstalled)
-  console.log('clis ===== ', env.getGeneratorNames())
-  console.log('res=====', env.run('fantu'))
-  console.log('createOptions===', createOptions)
+  // generator 安装失败
+  if (!isInstalled) {
+  }
+  // 项目初始化: yeoman 托管
+  const isInitialized = await env.run('fantu:react-mui').catch(() => false)
+  // 项目初始化失败
+  console.log('issssss====', isInitialized, chalk.red('111'))
 }
